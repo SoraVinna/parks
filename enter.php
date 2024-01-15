16343d477294d4a30d950f390ef4df19daf5bc44
SoraVinna <148188038+SoraVinna@users.noreply.github.com>
2024-01-15 09:27:06 +0200

    Update enter.php

--- enter.php
+++ enter.php
@@ -1,5 +1,16 @@
 <?php
 
+$servername = "127.0.0.1";
+$username = "root";
+$password = "yboQx5L4[15I5+";
+$dbname = "parks";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
